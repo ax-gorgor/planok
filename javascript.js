76640482e1ed04b94b644b8dd8abde00c89bf1dd
ax-gorgor <80730395+ax-gorgor@users.noreply.github.com>
2022-06-14 23:09:55 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,214 @@
-   
+
 var a1 = document.getElementById('a1');
 var a2 = document.getElementById('a2');
 var a3 = document.getElementById('a3');
+var a10 = document.getElementById('a10');
+var a10 = document.getElementById('a10');
+var a11 = document.getElementById('a11');
+var a12 = document.getElementById('a12');
+var a13 = document.getElementById('a13');
+var a14 = document.getElementById('a14');
+var a15 = document.getElementById('a15');
+var a16 = document.getElementById('a16');
+var a20 = document.getElementById('a20');
+
+var a100 = document.getElementById('a100');
+var a110 = document.getElementById('a110');
+var a120 = document.getElementById('a120');
+var a130 = document.getElementById('a130');
+var a140 = document.getElementById('a140');
+var a150 = document.getElementById('a150');
+var a160 = document.getElementById('a160');
+var b10 = document.getElementById('b10');
+var b11 = document.getElementById('b11');
+var b12 = document.getElementById('b12');
+var b13 = document.getElementById('b13');
+var b14 = document.getElementById('b14');
+var b15 = document.getElementById('b15');
+ 
 
 var getin = prompt ('Poné el artículo')
-if (getin!="1550" & getin!="1551" & getin!="1552")
- 
-{alert('Error de artículo')}
-else { a1.style.background= 'black'}
 
+if (getin!="1550" & getin!="1551" & getin!="1552" )
+
+{ }
+
+else { a1.style.background= 'red';
+      a1.style.border= '1px solid rgb(0, 0, 0)';
+      }
+
+ if (  getin!="2550" & getin!="2551" & getin!="2552")
  
+{ }
+
+else { 
+     a2.style.background= 'red';
+     a2.style.border= '1px solid rgb(0, 0, 0)';
+      }
+
+if (  getin!="3550" & getin!="3551" & getin!="3552")
+ 
+      { }
+      
+else { 
+      a3.style.background= 'red';
+      a3.style.border= '1px solid rgb(0, 0, 0)';
+      };
+
+if (  getin!="4550" & getin!="4551" & getin!="4552")
+ 
+{ }
+      
+else { 
+      a10.style.background= 'red';
+      a10.style.border= '1px solid rgb(0, 0, 0)';
+      };
+
+if (  getin!="5550" & getin!="5551" & getin!="5552")
+ 
+{ }
+            
+ else { 
+     a11.style.background= 'red';
+     a11.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+
+if (  getin!="6550" & getin!="6551" & getin!="6552")
+ 
+{ }
+            
+else { 
+     a12.style.background= 'red';
+     a12.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+if (  getin!="7550" & getin!="7551" & getin!="7552")
+ 
+{ }
+                 
+else { 
+     a13.style.background= 'red';
+     a13.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+if (  getin!="8550" & getin!="8551" & getin!="8552")
+ 
+{ }
+                      
+else { 
+     a14.style.background= 'red';
+     a14.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+if (  getin!="9550" & getin!="9551" & getin!="9552")
+ 
+{ }
+                           
+else { 
+     a15.style.background= 'red';
+    a15.style.border= '1px solid rgb(0, 0, 0)';
+    };
+
+    if (  getin!="10550" & getin!="10551" & getin!="10552")
+ 
+    { }
+                               
+    else { 
+         a16.style.background= 'red';
+        a16.style.border= '1px solid rgb(0, 0, 0)';
+        };
+
+//////////////////////////////////////////////////////////
+
+if (  getin!="200" & getin!="201" & getin!="202")
+ 
+{ }
+      
+else { 
+      a100.style.background= 'red';
+      a100.style.border= '1px solid rgb(0, 0, 0)';
+      };
+
+if (  getin!="300" & getin!="301" & getin!="302")
+ 
+{ }
+            
+ else { 
+     a110.style.background= 'red';
+     a110.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+
+if (  getin!="400" & getin!="401" & getin!="402")
+ 
+{ }
+            
+else { 
+     a120.style.background= 'red';
+     a120.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+if (  getin!="500" & getin!="501" & getin!="502")
+ 
+{ }
+                 
+else { 
+     a130.style.background= 'red';
+     a130.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+if (  getin!="600" & getin!="601" & getin!="602")
+ 
+{ }
+                      
+else { 
+     a140.style.background= 'red';
+     a140.style.border= '1px solid rgb(0, 0, 0)';
+     };
+
+if (  getin!="700" & getin!="701" & getin!="702")
+ 
+{ }
+                           
+else { 
+     a150.style.background= 'red';
+    a150.style.border= '1px solid rgb(0, 0, 0)';
+    };
+
+    if (  getin!="800" & getin!="801" & getin!="802")
+ 
+    { }
+                               
+    else { 
+         a160.style.background= 'red';
+        a160.style.border= '1px solid rgb(0, 0, 0)';
+        };
+
+if (  getin!="80" & getin!="81" & getin!="82")
+ 
+{ }
+                                   
+else { 
+    a20.style.background= 'red';
+    a20.style.border= '1px solid rgb(0, 0, 0)';
+    };
+ ////////////////////////////////////
+
+ if (  getin!="1110" & getin!="1111" & getin!="1112")
+ 
+{ }
+                                   
+else { 
+    b10.style.background= 'red';
+    b10.style.border= '1px solid rgb(0, 0, 0)';
+    };
+
+    if (  getin!="2110" & getin!="2111" & getin!="2112")
+ 
+    { }
+                                       
+    else { 
+        b11.style.background= 'red';
+        b11.style.border= '1px solid rgb(0, 0, 0)';
+        };
